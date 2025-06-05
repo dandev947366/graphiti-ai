a1430a3e22de28365e20519985f40b10a8c3671b
Dan <dan.dev947366@gmail.com>
2025-06-05 23:26:54 +0300

    test(db): add test embedding to db

--- main/llm_embedder.py
+++ main/llm_embedder.py
@@ -4,12 +4,22 @@ from graphiti_core.nodes import EpisodeType
 from graphiti_core.embedder.client import EmbedderClient, EmbedderConfig
 from datetime import datetime
 from sentence_transformers import SentenceTransformer, util
+import os
+from dotenv import load_dotenv
+import singlestoredb as s2
+import numpy as np
+
+load_dotenv()
 
 DEFAULT_EMBEDDING_MODEL = "dunzhang/stella_en_1.5B_v5"
 
 
+def floats_to_blob(floats):
+    arr = np.array(floats, dtype=np.float32)
+    return arr.tobytes()
+
+
 class HuggingFaceEmbedderConfig(EmbedderConfig):
-    # embedding_model: EmbeddingModel | str = DEFAULT_EMBEDDING_MODEL
     embedding_model: str = DEFAULT_EMBEDDING_MODEL
     api_key: str | None = None
     base_url: str | None = None
@@ -25,14 +35,24 @@ class HuggingFaceEmbedder(EmbedderClient):
             config = HuggingFaceEmbedderConfig()
         self.config = config
         self.model = SentenceTransformer(config.embedding_model)
-        # self.client = AsyncOpenAI(api_key=config.api_key, base_url=config.base_url)
+        self.conn = s2.connect(os.environ.get("SINGLESTORE_URL"))
 
-    async def create(
-        self, input: str | List[str] | Iterable[int] | Iterable[Iterable[int]]
-    ) -> list[float]:
-        print(f"embbed this: {input}")
+    @staticmethod
+    def blob_to_floats(blob):
+        return np.frombuffer(blob, dtype=np.float32)
+
+    async def create(self, input):
         embeddings = self.model.encode(input)
-        print(f"type of embbeding: {type(embeddings)}")
-        print(f"type of embbeding shape: {type(embeddings.shape)}")
-        print(f"embbeding to return: {embeddings.shape}")
-        return embeddings.shape
+        with self.conn.cursor() as cur:
+            if isinstance(input, str):
+                texts = [input]
+                embeddings = [embeddings]
+            else:
+                texts = input
+
+            for text, emb in zip(texts, embeddings):
+                vector_blob = floats_to_blob(emb)
+                sql = "INSERT INTO myvectortable (text, vector) VALUES (%s, %s)"
+                cur.execute(sql, (text, vector_blob))
+        self.conn.commit()  # Make sure to commit
+        return embeddings
